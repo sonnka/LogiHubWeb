b748a2fa943232eeda0f78aa770d9d5585a247a7
sonnka <sonyakazanceva1331@gmail.com>
2024-06-05 05:44:21 +0300

    Added update profile page

--- src/app/_components/_general/update-profile/update-profile.component.ts
+++ src/app/_components/_general/update-profile/update-profile.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TruckManagerDTO} from "../../../_models/response/truck-manager-dto";
+import {ParkingManagerDTO} from "../../../_models/response/parking-manager-dto";
+import {LoginService} from "../../../_services/login.service";
+import {TruckManagerService} from "../../../_services/truck-manager.service";
+import {ParkingManagerService} from "../../../_services/parking-manager.service";
+import {Router} from "@angular/router";
+import {UtilService} from "../../../_services/util.service";
+import {ManagerRequest} from "../../../_models/request/manager-request";
 
 @Component({
   selector: 'app-update-profile',
@@ -6,5 +14,56 @@ import { Component } from '@angular/core';
   styleUrl: './update-profile.component.css'
 })
 export class UpdateProfileComponent {
+  protected truckManager: TruckManagerDTO | undefined;
+  protected parkingManager: ParkingManagerDTO | undefined;
+  protected readonly LoginService = LoginService;
 
+  constructor(private truckManagerService: TruckManagerService,
+              private parkingManagerService: ParkingManagerService,
+              private router: Router) {
+  }
+
+  ngOnInit() {
+    if (LoginService.isTruckManager()) {
+      this.getTruckManager();
+    } else if (LoginService.isParkingManager()) {
+      this.getParkingManager();
+    } else {
+      LoginService.logout(this.router)
+    }
+  }
+
+  protected async updateTruckManagerProfile(avatar: string, firstName: string, lastName: string) {
+    try {
+      await this.truckManagerService.updateManager(new ManagerRequest(avatar, firstName, lastName));
+    } catch (error) {
+      UtilService.displayError(error, this.router)
+    }
+    await this.router.navigate(['/truck-manager']);
+  }
+
+  protected async updateParkingManagerProfile(avatar: string, firstName: string, lastName: string) {
+    try {
+      await this.parkingManagerService.updateManager(new ManagerRequest(avatar, firstName, lastName));
+    } catch (error) {
+      UtilService.displayError(error, this.router)
+    }
+    await this.router.navigate(['/parking-manager']);
+  }
+
+  private async getTruckManager() {
+    try {
+      this.truckManager = await this.truckManagerService.getManager();
+    } catch (error) {
+      UtilService.displayError(error, this.router)
+    }
+  }
+
+  private async getParkingManager() {
+    try {
+      this.parkingManager = await this.parkingManagerService.getManager();
+    } catch (error) {
+      UtilService.displayError(error, this.router)
+    }
+  }
 }
--- src/app/_services/truck-manager.service.ts
+++ src/app/_services/truck-manager.service.ts
@@ -4,6 +4,8 @@ import {LoginService} from "./login.service";
 import {Router} from "@angular/router";
 import {RegisterRequest} from "../_models/request/register-request";
 import {CompanyDTO} from "../_models/response/company-dto";
+import {ManagerRequest} from "../_models/request/manager-request";
+import {TruckManagerDTO} from "../_models/response/truck-manager-dto";
 
 @Injectable({
   providedIn: 'root'
@@ -35,5 +37,43 @@ export class TruckManagerService {
       })
     };
     return this.http.get<CompanyDTO[]>(this.baseUrl + '/truck-company/search', httpOptions);
+  }
+
+  async getManager() {
+    await this.getCredentials()
+
+    return await this.http.get<TruckManagerDTO>(
+      this.baseUrl + '/truck-manager/' + this.id, this.httpOptions).toPromise();
+  }
+
+  async updateManager(managerRequest: ManagerRequest) {
+    await this.getCredentials()
+
+    return await this.http.patch<void>(this.baseUrl + "/truck-manager/" + this.id, managerRequest,
+      this.httpOptions).toPromise()
+  }
+
+  async deleteManager() {
+    await this.getCredentials();
+
+    return await this.http.delete(this.baseUrl + '/truck-manager/' + this.id, this.httpOptions)
+      .toPromise();
+  }
+
+  private async getCredentials() {
+    this.token = this.loginService.getToken()
+    this.id = this.loginService.getUserId()
+
+    if (!this.token || !this.id) {
+      await LoginService.logout(this.router);
+      throw new Error("Something went wrong!")
+    }
+
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.token
+      })
+    };
   }
 }
