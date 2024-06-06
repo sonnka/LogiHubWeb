339bfcdc2a8995ac22e69a7738c2469cae366261
sonnka <sonyakazanceva1331@gmail.com>
2024-06-06 05:03:08 +0300

    Added truck page

--- src/app/_components/_truck-manager/truck/truck.component.ts
+++ src/app/_components/_truck-manager/truck/truck.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {UtilService} from "../../../_services/util.service";
+import {TruckDTO} from "../../../_models/response/truck-dto";
+import {TruckManagerService} from "../../../_services/truck-manager.service";
 
 @Component({
   selector: 'app-truck',
@@ -6,5 +10,30 @@ import { Component } from '@angular/core';
   styleUrl: './truck.component.css'
 })
 export class TruckComponent {
+  protected truck: TruckDTO | undefined;
 
+  constructor(private truckManagerService: TruckManagerService, private router: Router, private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id')!;
+    this.getTruck(id);
+  }
+
+  protected async updateTruckByIot() {
+    try {
+      this.truck = await this.truckManagerService.updateTruckByIOT();
+    } catch (error) {
+      UtilService.displayError(error, this.router)
+    }
+    window.location.reload();
+  }
+
+  private async getTruck(id: number) {
+    try {
+      this.truck = await this.truckManagerService.getTruck(id);
+    } catch (error) {
+      UtilService.displayError(error, this.router)
+    }
+  }
 }
